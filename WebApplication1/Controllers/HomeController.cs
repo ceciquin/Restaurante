1b046c654ebb3939e83b990de08ab36b66423914
Quintana Amarilla <c.quintana.amarilla@accenture.com>
2018-11-27 17:11:17 -0300

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace WebApplication1.Controllers
+{
+    public class    HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //return View();
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Login()
+        {
+            if (Session["Usuario"] != null)
+            {
+                return RedirectToAction("UserDashBoard");
+            }
+            return View();
+        }
+
+        public ActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(PerfilUsuario objUser)
+        {
+            if (ModelState.IsValid)
+            {
+                using (restauranteEntities db = new restauranteEntities())
+                {
+                    var obj = db.PerfilUsuario.Where(a => a.Usuario.Equals(objUser.Usuario) && a.Password.Equals(objUser.Password)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        obj.IsActive = true;
+                        db.SaveChanges();
+
+                        Session["IdUsuario"] = obj.IdUsuario.ToString();
+                        Session["Usuario"] = obj.Usuario.ToString();
+                        Session["isActive"] = obj.IsActive;
+                        FormsAuthentication.SetAuthCookie(Session["Usuario"].ToString(), true);
+                        return RedirectToAction("UserDashBoard");
+                        
+                    } else
+                    {
+                        ModelState.AddModelError("LoginFail", "Datos incorrectos");
+                        return View();
+                    }
+                }
+            }
+            return View(objUser);
+        }
+
+        public ActionResult UserDashBoard()
+        {
+            if (Session["IdUsuario"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogOff()
+        {
+            Session.Clear();
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+
+    }
+}
---
+++ WebApplication1/Scripts/Validaciones.js
@@ -0,0 +1,34 @@
+﻿
+function recarga() {
+    document.getElementById("provincia").value = "";
+    document.getElementById("terminos").checked = false;
+    document.getElementById("apellido").value = "";
+    document.getElementById("nombre").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("dni").value = "";
+}
+
+function validarLocalidades() {
+    var provincia = document.getElementById("provincia").value;
+    if (provincia == "bsas") {
+        document.getElementById("localidades").style.display = "block";
+    } else {
+        document.getElementById("localidades").style.display = "none";
+    }
+}
+
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+})
+
+function validarEnvio() {
+    var provincia = document.getElementById("provincia").value;
+    var terminos = document.getElementById("terminos").checked;
+    var apellido = document.getElementById("apellido").value;
+    var nombre = document.getElementById("nombre").value;
+    var email = document.getElementById("email").value;
+    var dni = document.getElementById("dni").value;
+    if (terminos || provincia == null || apellido == null || nombre == null || email == null || dni == null) {
+        alert('[ERROR] Ingrese correctamente los campos a completar');
+    }
+}
